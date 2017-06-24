2ee3ebacdb19c72ba7b0b31485bb4194c564a471
Igor Kamyshev <garik.novel@gmail.com>
2017-06-24 23:51:36 +0700

    task 1.9 v3

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def app(env, start_response):
+def app(environ, start_response):
 	body = environ['QUERY_STRING'].split('&')
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return body
+	start_response('200 OK', [('Content-Type', 'text/plain')])
+	return body
