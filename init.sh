3945739cba79d87ecb1c6e964b26da2aff697b3c
Igor Kamyshev <garik.novel@gmail.com>
2017-06-24 14:17:58 +0700

    task 1.8 v3

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/django.conf
-sudo -s rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
